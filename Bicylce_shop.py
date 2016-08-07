3bf3e2a9b19eec6c1a3001b21131d0c12313cdb2
Joe Leahy <josleahy@gmail.com>
2016-08-07 11:42:04 -0400

    Initial Commit

--- Bicylce_shop.py
+++ Bicylce_shop.py
@@ -1,21 +1,19 @@
 class Bicycle(object):
     def __init__(self, model, weight, cost_produce):
         self.model = ''
-        self.weight = 0
-        self.cost_produce = 0
+        self.weight= 0
+        self.cost_produce = 200
 
 class Bike_Shops(object):
-    def __init__(self, name, inventory, markup):
+    def __init__(self):
         self.name = "Joe's Bike Shop"
         self.inentory = 0
-        self.markup = Bicycle.cost_produce + (Bicycle.cost_produce * .20)
+        self.markup = .20
+    def check_earnings(self):
+
 
 class Customer(object):
-    def __init__(self, name, money,):
+    def __init__(self):
         self.name = ""
-        self.money = 0
-
-
+        self.money = 500
     def buy_bike(self):
-        #Customer money becomes money - cost of the bike
-        #Bike_Shop inventory - 1
