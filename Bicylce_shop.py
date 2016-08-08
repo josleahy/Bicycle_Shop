8285418898b7038296a52c18305d75b9accd4ae0
Joe Leahy <josleahy@gmail.com>
2016-08-07 20:04:22 -0400

    Increasing Functionality

--- Bicylce_shop.py
+++ Bicylce_shop.py
@@ -16,4 +16,21 @@ Customer("Mary",1000),
 Customer("Rob", 500),
 ]
 
-print("Welcome to " + shop.name + "!")
+print ("Our Current Inventory")
+print ("-" * 20)
+for bike in range(len(shop_inventory)):
+     print (shop_inventory[bike].name)
+print ("-" * 20)
+
+print("Customers")
+print("-" * 20)
+for customer in range(len(customers)):
+    print(customers[customer].name)
+
+print("-" * 20)
+print("Who can afford what?")
+print("-" * 20)
+for customer in range(len(customers)):
+    for bike in range(len(shop_inventory)):
+        if shop_inventory[bike].cost_produce <= customers[customer].money:
+            print ("{0} can afford the: {1}".format(customers[customer].name, shop_inventory[bike].name))
